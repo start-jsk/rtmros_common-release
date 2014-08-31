63c58cf63ecd3064ad0a7b0e5d05f431b3d8206b
Isaac IY Saito <130s@lateeye.net>
2014-08-31 10:45:27 +0900

    Trimmed the branch to only the hrpsys_ros_bridge sub directory

--- rosnode_rtc/sample/tkjoy_comp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -i
-
-roscd openrtm/build/OpenRTM-aist-Python-1.0.1/OpenRTM_aist/examples/TkJoyStick
-PYTHONPATH=$PYTHONPATH:`rospack export --lang=python --attrib=path openrtm | cut -d\  -f1` python TkJoyStickComp.py
